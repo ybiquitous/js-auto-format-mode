e5074a25d256c9e9e851256b9a535afb337dabb0
ybiquitous <ybiquitous@gmail.com>
2017-10-15 01:11:54 +0900

    add easy usage to source file

--- js-auto-format-mode.el
+++ js-auto-format-mode.el
@@ -26,7 +26,15 @@
 
 ;;; Commentary:
 
-;; Please see `https://github.com/ybiquitous/js-auto-format-mode'.
+;; Usage:
+;;
+;;   (add-hook 'js-mode-hook #'js-auto-format-mode)
+;;
+;; To customize:
+;;
+;;   M-x customize-group RET js-auto-format RET
+;;
+;; For details, please see `https://github.com/ybiquitous/js-auto-format-mode'.
 
 ;;; Code:
 
